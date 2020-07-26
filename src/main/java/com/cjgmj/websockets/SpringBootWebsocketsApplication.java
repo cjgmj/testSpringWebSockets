5f3838185c0c140dcd6d038bc70c56ed0cac5e08
cjgmj <cjgmj@hotmail.com>
2020-07-26 12:45:01 +0200

    Proyecto base

---
+++ src/main/java/com/cjgmj/websockets/SpringBootWebsocketsApplication.java
@@ -0,0 +1,13 @@
+package com.cjgmj.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketsApplication.class, args);
+	}
+
+}
